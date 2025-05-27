910a7a55486f6d4cbe1504c982e6e78f12df7ee6
kytran-saritasa <ky.tran@saritasa.com>
2025-05-27 11:49:34 +0700

    Improve table row

--- src/features/books/BooksTableRow.jsx
+++ src/features/books/BooksTableRow.jsx
@@ -4,80 +4,70 @@ import { HiPencil, HiTrash } from 'react-icons/hi2';
 import { formatISBN13, isValidIsbn13 } from '~/utils';
 import { DashChar, Modal, Table, TableMenu } from '~/components';
 import { bookShape } from '~/types';
-
-import {
-  BookDelete,
-  BooksForm,
-  BooksTableRowButton,
-  BooksTableRowMenuButton,
-} from '~/features/books';
+import { BookDelete, BooksForm, BooksTableRowButton, BooksTableRowMenuButton } from '~/features/books';
 
 function BooksTableRow({ book }) {
   const { id, name, authors, publicationYear, rating, isbn } = book;
-
   if (isbn && !isValidIsbn13(isbn)) throw new Error('Invalid ISBN!');
-
-  // 0 && 1 -> 0 -> Only format ISBN if ISBN exists
   const formattedISBN = isbn && formatISBN13(isbn);
 
-  if (!isValidIsbn13())
-    return (
-      <Table.Row>
-        <BookImageUI src="/images/book.jpeg" alt="Saritasa book item" />
+  return (
+    <Table.Row>
+      <BookImageUI src="/images/book.jpeg" alt={`Book: ${book.name}`} />
 
-        <BookNameUI>{name}</BookNameUI>
-        <DashChar priorityValue={authors.join(', ')} UI={BookNameUI} />
-        <DashChar priorityValue={publicationYear} />
-        <DashChar priorityValue={rating} />
-        <DashChar priorityValue={formattedISBN} />
+      <BookNameUI>{name}</BookNameUI>
+      <DashChar priorityValue={authors.join(', ')} UI={BookNameUI} />
+      <DashChar priorityValue={publicationYear} />
+      <DashChar priorityValue={rating} />
+      <DashChar priorityValue={formattedISBN} />
 
-        <Modal>
-          <TableMenu>
-            <TableMenu.Toggle
-              id={id}
-              renderButton={({ active, onClick }) => (
-                <BooksTableRowButton active={active} onClick={onClick} />
-              )}
-            />
+      <Modal>
+        <TableMenu>
+          <TableMenu.Toggle
+            id={id}
+            renderButton={({ active, onClick }) => (
+              <BooksTableRowButton active={active} onClick={onClick} />
+            )}
+          />
 
-            <TableMenu.List
-              id={id}
-              buttons={[
-                { icon: HiPencil, label: 'Edit', value: 'edit' },
-                { icon: HiTrash, label: 'Delete', value: 'delete' },
-              ]}
-              renderButton={({ icon, label, value, onCloseModal }) => (
-                <Modal.Open openName={value}>
-                  <BooksTableRowMenuButton
-                    icon={icon}
-                    label={label}
-                    onClick={onCloseModal}
-                  />
-                </Modal.Open>
-              )}
-            ></TableMenu.List>
-
-            <Modal.Window
-              openName="edit"
-              renderWindow={({ onCloseModal }) => (
-                <BooksForm
-                  type="edit"
-                  isMultipleTimes={false}
-                  bookToEdit={book}
-                  onCloseForm={onCloseModal}
+          <TableMenu.List
+            id={id}
+            buttons={[
+              { icon: HiPencil, label: 'Edit', value: 'edit' },
+              { icon: HiTrash, label: 'Delete', value: 'delete' },
+            ]}
+            renderButton={({ icon, label, value, onCloseModal }) => (
+              <Modal.Open openName={value}>
+                <BooksTableRowMenuButton
+                  icon={icon}
+                  label={label}
+                  onClick={onCloseModal}
                 />
-              )}
-            />
-            <Modal.Window
-              openName="delete"
-              renderWindow={({ onCloseModal }) => (
-                <BookDelete bookID={id} onCloseModal={onCloseModal} />
-              )}
-            />
-          </TableMenu>
-        </Modal>
-      </Table.Row>
-    );
+              </Modal.Open>
+            )}
+          ></TableMenu.List>
+
+          <Modal.Window
+            openName="edit"
+            renderWindow={({ onCloseModal }) => (
+              <BooksForm
+                type="edit"
+                isMultipleTimes={false}
+                bookToEdit={book}
+                onCloseForm={onCloseModal}
+              />
+            )}
+          />
+          <Modal.Window
+            openName="delete"
+            renderWindow={({ onCloseModal }) => (
+              <BookDelete bookID={id} onCloseModal={onCloseModal} />
+            )}
+          />
+        </TableMenu>
+      </Modal>
+    </Table.Row>
+  );
 }
 
 const BookImageUI = styled.img`
